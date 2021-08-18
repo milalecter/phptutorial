b192adafe6b4e4f98d3dae17722422f1cb53b9fc
milalecter <akiyama.mila@hotmail.com>
2021-08-18 22:06:25 +0200

    Update footer.php
    new button

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,9 @@
+        
         <?php
+            echo "<br/>";
             echo "<p>Copyright (づ｡◕‿‿◕｡)づ - please don't copy - (*ฅ́˘ฅ̀*) .｡.:*♡ -" . date("Y") . "- PHP Masterclass</p>";
         ?>
+        <a href="https://www.heroku.com" class="btn btn-info">I'm using Heroku to host this site!</a>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
     </div>
     </body>
